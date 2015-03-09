5f5a7bce325fcbce2745d20ab1c8d03adea131ff
Jochen Brissier <j.brissier@me.com>
2015-03-09 16:46:16 +0100

    adding messages api to urls

--- base/urls.py
+++ base/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 from django.views.generic import TemplateView
+from chat.views import SimpleMessageChat
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # admin page
-    url(r'^$', TemplateView.as_view(template_name='home.html')),
-
+    url(r'^$', TemplateView.as_view(template_name='chat/home.html')),
+    url(r'^messages/$', SimpleMessageChat.as_view(), name="message_backend"),
     url(r'^admin/', include(admin.site.urls))
 )
