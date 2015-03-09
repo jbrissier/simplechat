9b6428d217e24e80323829d31a3cd0c3cdcf7b2a
Jochen Brissier <j.brissier@gmail.com>
2015-03-09 22:32:55 +0100

    adding channel

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Channel(models.Model):
+    name = models.CharField(max_length=255)
+    members = models.ManyToManyField(User)
